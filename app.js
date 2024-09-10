f77f3a3c79270ca005c7ea1713295670af2141f7
AmalJoy14 <amaljoy246@gmail.com>
2024-09-10 19:33:02 +0530

    done

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 import mongoose from "mongoose";
 import express from "express";
+import passport from "passport";
+import localStrategy from "passport-local";
+import expressSession from "express-session";
+import flash from "connect-flash";
+import cookieParser from "cookie-parser";
+import device from 'express-device';
 
 const app = express();
 const PORT = 3000;
@@ -8,7 +14,31 @@ const PORT = 3000;
 
 app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.json());
+app.use(flash());
+app.use(device.capture());
+
+////////////////////////////////////////////////////////////
+passport.use(new localStrategy(user.authenticate()));
+app.use(
+  expressSession({
+    resave: false,
+    saveUninitialized: true,
+    secret: "hey",  
+    cookie: { secure: false, maxAge: 86400000 } 
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(user.deserializeUser());
+
+//////////////////////////////////////////////////////////////////////
 
 
 app.get("/", (req, res) => {
