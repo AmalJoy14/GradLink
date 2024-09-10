2e4bd721d2d66c22b71a8286243580625c553d76
AmalJoy14 <amaljoy246@gmail.com>
2024-09-10 19:13:28 +0530

    done

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+
+
 app.set("view engine", "ejs");
+
+app.use(express.static('public'));
+
 
 app.get("/", (req, res) => {
     res.render("start.ejs");
 });
+
+app.get("/home", (req,res) =>{
+    res.render("login.ejs");
+})
 
 app.listen(PORT, ()=>{
     console.log(`Listening to port ${PORT}`);
